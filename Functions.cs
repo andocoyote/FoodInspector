c31c561fc4a743de7f61bdab385877b3743b69a5
Andrew Mikesell <andocoyote@outlook.com>
2022-12-10 15:45:29 -0800

    Initial commit of FoodInspector solution

---
+++ Functions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodInspector
+{
+    public class Functions
+    {
+        public static void ProcessMessageOnTimer(
+            [TimerTrigger("0 */15 * * * *", RunOnStartup = true)] TimerInfo timerInfo,
+            ILogger logger)
+        {
+            logger.LogInformation("TimerTrigger fired.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+// Creating WebJobs that target .NET
+// https://learn.microsoft.com/en-us/azure/app-service/webjobs-sdk-get-started
+
+namespace FoodInspector
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder();
+
+            // The AddConsole method adds console logging to the configuration
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+
+            // The ConfigureWebJobs extension method initializes the WebJobs host
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddTimers();
+            });
+
+            var host = builder.Build();
+
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
